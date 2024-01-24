d00d48f41b55f490a615b69f8f3ec503cc244e69
Huzaifa_q <huzaifaquadri1853@gmail.com>
2024-01-24 20:47:59 +0530

    Animation Branch : Just a cleanup lol

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,51 +1,3 @@
-// import 'package:flutter/foundation.dart';
-// import 'package:flutter/material.dart';
-// import 'package:meals/data/dummy_data.dart';
-// import 'package:meals/screens/meals.dart';
-// import 'package:meals/widjets/category_griditem.dart';
-
-// class CategoriesScreen extends StatelessWidget {
-//   const CategoriesScreen({super.key});
-
-//   void _selectedCategory(BuildContext context, Category category){
-
-//     final filteredMeals = dummyMeals.where(
-//       (meal) => meal.categories.contains(category.id)
-//       ).toList();
-
-//     Navigator.push(context, MaterialPageRoute(       //* Alt : Navigator.of(context).push(route)
-//       builder: (ctx) => Meals(
-//         title: category.title,
-//         meals: filteredMeals,
-//       )
-//     ));
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text("Meals"),
-//       ),
-//       body: GridView(
-//         padding: const EdgeInsets.all(16),
-//         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-//             crossAxisCount: 2,
-//             childAspectRatio: 3 / 2,
-//             crossAxisSpacing: 20,
-//             mainAxisSpacing: 10
-//           ),
-
-//         children:  [
-//           for(final category in availableCategories)
-//             CategoryItem(category: category, onSelectCategory: (){
-//               _selectedCategory(context, category);
-//             })
-//         ],
-//       ),
-//     );
-//   }
-// }
 
 import 'package:flutter/material.dart';
 
@@ -106,3 +58,6 @@ class CategoriesScreen extends StatelessWidget {
     // );
   }
 }
+
+
+//! THIS IS ANIMATION BRANCH 
